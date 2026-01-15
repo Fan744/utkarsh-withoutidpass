ec9189090c0d462cde04abdf3443690b955ba19c
Fan744 <135441979+Fan744@users.noreply.github.com>
2026-01-15 22:14:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+API_ID = "20401115"
+API_HASH = "f27d722a2772cc581435be607be35022"
+BOT_TOKEN = "7970441104:AAEiaUoJb0CG-rdz8eFDha5KYwAd6F6IfjY"
+
+OWNER = "7955217692"
+
+# Agar DB ka naam chahiye to yaha define kar sakte ho
+DB_NAME = "Cluster0"
+
+# Mongo URL yaha daal do
+MONGO_URL = "mongodb+srv://Rajaluck900:Rajaluck900@cluster0.f0gdi0d.mongodb.net/?appName=Cluster0"
